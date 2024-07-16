074aff56ba449d373dced4ae246d05e57037cc99
Jesper Christensen <YB4I@ftn.eurofins.com>
2024-07-16 15:21:41 +0200

    Finished project code

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+
+
 def word_count(Book):
     letters = {}
     lower_book = Book.lower()
@@ -9,9 +11,10 @@ def word_count(Book):
     return letters
 
 def letter_sort(let_dict):
+    let_dict = dict(sorted(let_dict.items(), key=lambda item: item[1],reverse=True))
     for letter in let_dict:
-        if letter.isalpha() == True:
-            print(letter)
+        if letter.isalpha():
+            print(f"the '{letter}' character was found {let_dict[letter]} times")
 
 def main():
     out = 0
@@ -24,6 +27,7 @@ def main():
         
     print(f"{out} words found in the decument")
     letter_sort(word_count(file_contents))
+    print("--- End report ---")
             
     
 
